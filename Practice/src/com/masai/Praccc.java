34a10a7c9fb35c0fb666c894b07444b57a7262a0
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-08-12 23:07:37 +0530

    check

---
+++ Practice/src/com/masai/Praccc.java
@@ -0,0 +1,27 @@
+package com.masai;
+
+
+//import java.util.*;
+public class Praccc extends Pracc2{
+	
+	public void fun1() {
+		System.out.println("Inside Fun1 Prac");
+	}
+
+	public static void main(String[] args) {
+		
+		Praccc obj= (Praccc)new Pracc2();
+		obj.fun1();
+		
+		//int arr[]= {0,1};
+
+		//System.out.println(Arrays.toString(arr));
+		
+		
+		
+
+	}
+
+
+
+}
