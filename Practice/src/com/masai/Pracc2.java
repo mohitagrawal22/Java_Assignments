d573b9294974cf06ab3542c72474fcc6339ba1d0
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-08-12 23:07:09 +0530

    check

---
+++ Practice/src/com/masai/Pracc2.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class Pracc2 {
+	
+	public void fun1() {
+		System.out.println("Inside Fun1 of prac2");
+	}
+
+	public static void main(String[] args) {
+		Pracc2 obj2=new Praccc();
+		//obj.fun2();
+	}
+	
+}
