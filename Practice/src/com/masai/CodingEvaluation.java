183068a4db3bb4e9237b0e59a964ace8ed0cbaa7
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-08-21 22:00:52 +0530

    added

---
+++ Practice/src/com/masai/CodingEvaluation.java
@@ -0,0 +1,14 @@
+package com.masaiEvalC3;
+
+public class CodingEvaluation extends Evaluation{
+
+	public CodingEvaluation(int question) {
+		super(question);
+	}
+
+	@Override
+	void evaluationTiming() {
+		System.out.println("Evaluation timing is 2:00 to 3:30");
+	}
+	
+}
