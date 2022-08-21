6e6e31a34679b79941f764944e32063a251eb3ea
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-08-21 22:01:40 +0530

    added

---
+++ Practice/src/com/masai/DsaEvaluation.java
@@ -0,0 +1,14 @@
+package com.masaiEvalC3;
+
+public class DsaEvaluation extends Evaluation{
+	
+	@Override
+	void evaluationTiming() {
+         System.out.println("Evaluation timing is 9:30 to 11:00");
+	}
+	
+	public DsaEvaluation(int question) {
+		super(question);
+	}
+
+}
